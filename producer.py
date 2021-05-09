28e7540f81c1eb2082ae44b65d30be1949340d79
Bar Snir <bars@Admins-MacBook-Pro.local>
2021-05-09 16:55:33 +0300

    added producer & consumer with confluent cloud

--- producer.py
+++ producer.py
@@ -1,3 +1,58 @@
+import os
+import json
+from pathlib import Path
+from dotenv import load_dotenv
+from confluent_kafka import Producer
+
+delivered_records = 0
 
 def full_flow_producer():
-    pass
+    load_envs()
+    producer = get_producer()
+    num_of_messages = 10
+    send_messages(producer, num_of_messages)
+    producer.flush()
+
+def load_envs():
+    env_path = Path('.') / '.env'
+    load_dotenv(dotenv_path=env_path)
+
+def get_producer():
+    producer_config = get_producer_config()
+    return  Producer(producer_config)
+
+def get_producer_config():
+    host = os.getenv('KAFKA_HOST')
+    kafka_username = os.getenv('KAFKA_USER')
+    kafka_password = os.getenv('KAFKA_PASS')
+    return {
+        'bootstrap.servers': host, 
+        'security.protocol': 'SASL_SSL', 
+        'sasl.mechanisms': 'PLAIN', 
+        'sasl.username': kafka_username, 
+        'sasl.password': kafka_password
+    }
+
+def serve_ack_status(err, msg):
+    global delivered_records
+    """Delivery report handler called on
+    successful or failed delivery of message
+    """
+    if err is not None:
+        print("Failed to deliver message: {}".format(err))
+    else:
+        delivered_records += 1
+        print("Produced record to topic {} partition [{}] @ offset {}"
+                .format(msg.topic(), msg.partition(), msg.offset()))
+
+def send_messages(producer, num_of_messages):
+    topic = 'test_topic'
+    for n in range(num_of_messages):
+        record_key = "alice"
+        record_value = json.dumps({'count': n})
+        partition_key = f"{record_key}_{n}"
+        print("Producing record: {}\t{}".format(partition_key, record_value))
+        producer.produce(topic, key=partition_key, value=record_value, on_delivery=serve_ack_status)
+        producer.poll(0)
+
+full_flow_producer()
---
+++ simple-serivce-consumer.py
@@ -0,0 +1,73 @@
+import os
+import json
+from pathlib import Path
+from dotenv import load_dotenv
+from confluent_kafka import Consumer
+
+def full_flow_producer():
+    load_envs()
+    consumer = get_kafka_consumer()
+    subscribe(consumer)
+    consume(consumer)
+
+def load_envs():
+    env_path = Path('.') / '.env'
+    load_dotenv(dotenv_path=env_path)
+
+def get_kafka_consumer():
+    consumer_config = get_kafka_consumer_config()
+    return Consumer(consumer_config)
+
+def get_kafka_consumer_config():
+    host = os.getenv('KAFKA_HOST')
+    kafka_username = os.getenv('KAFKA_USER')
+    kafka_password = os.getenv('KAFKA_PASS')
+    group_id = 'simple-service-A'
+    protocol = 'SASL_SSL'
+    mechanisms =  'PLAIN'
+    auto_offset_reset = 'earliest'
+    return {
+        'bootstrap.servers': host,
+        'security.protocol': protocol,
+        'sasl.mechanisms': mechanisms,
+        'sasl.username': kafka_username,
+        'sasl.password': kafka_password,
+        'group.id': group_id,
+        'auto.offset.reset': auto_offset_reset,
+    }
+
+def subscribe(consumer):
+    topic = 'test_topic'
+    consumer.subscribe([topic])
+
+def consume(consumer):
+    total_count = 0
+    try:
+        while True:
+            msg = consumer.poll(1.0)
+            if msg is None:
+                # No message available within timeout.
+                # Initial message consumption may take up to
+                # `session.timeout.ms` for the consumer group to
+                # rebalance and start consuming
+                print("Waiting for message or event/error in poll()")
+                continue
+            elif msg.error():
+                print('error: {}'.format(msg.error()))
+            else:
+                # Check for Kafka message
+                record_key = msg.key()
+                record_value = msg.value()
+                data = json.loads(record_value)
+                count = data['count']
+                total_count += count
+                print("Consumed record with key {} and value {}, \
+                      and updated total count to {}"
+                      .format(record_key, record_value, total_count))
+    except KeyboardInterrupt:
+        pass
+    finally:
+        # Leave group and commit final offsets
+        consumer.close()
+
+full_flow_producer()    
